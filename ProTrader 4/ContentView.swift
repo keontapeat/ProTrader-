e382f130f3ea283c6d5f39bfd0421244c74fbe7b
keontapeat <keontapeat@gmail.com>
2025-02-18 10:09:55 -0500

    Initial Commit

---
+++ ProTrader 4/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ProTrader 4
+//
+//  Created by Keonta  on 2/18/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ProTrader 4/ProTrader_4App.swift
@@ -0,0 +1,17 @@
+//
+//  ProTrader_4App.swift
+//  ProTrader 4
+//
+//  Created by Keonta  on 2/18/25.
+//
+
+import SwiftUI
+
+@main
+struct ProTrader_4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
